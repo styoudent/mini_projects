c662395f07e3cb09edec74dbc7d36bbf401ec0e9
styoudent <styoudent@int.pl>
2023-03-21 11:26:53 +0100

    deleted too complicated upgrade

--- modules/Tic_Tac_Toe/src/main/game/Game.java
+++ modules/Tic_Tac_Toe/src/main/game/Game.java
@@ -1,43 +1,15 @@
 package main.game;
-
-import main.board.Board;
-
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Game {
 
-    public void initializeGame() throws IOException {
-        long maxWaitTimeInMilliseconds = 10000;
-        long elapsedTimeInMilliseconds = 0;
-        long startTimeInMilliseconds = System.currentTimeMillis();
-        boolean isInputReceived = false;
-        String userInputBoardSize = null;
+    public void initializeGame(){
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Enter the size of game board in format \"X Y\" ");
-        try {
-            while (!isInputReceived && elapsedTimeInMilliseconds<maxWaitTimeInMilliseconds) {
-                if (System.in.available() > 0) {
-                    userInputBoardSize = scanner.nextLine();
-                    isInputReceived = true;
-                }
-                elapsedTimeInMilliseconds = System.currentTimeMillis() - startTimeInMilliseconds; // aktualizacja czasu, który minął
-            }
-        }
-        catch (IOException ioe) {
-            System.out.println(ioe);
-
-        }
-        if (isInputReceived) {
-            System.out.println("Dane wejściowe zostały wprowadzone: " + userInputBoardSize);
-//            board.
-        }
-        else {
-            System.out.println("Nie wprowadzono danych wejściowych w ciągu " + maxWaitTimeInMilliseconds + " ms.");
-        }
+        String userInputBoardSize = scanner.nextLine();
         scanner.close();
-
+        System.out.println(userInputBoardSize);
+//        userInputBoardSize.
 //        Board board = new Board();
 //        board.setBoard(stringFromInput);
 //        board.fillBoardWithDefaultSigns();
